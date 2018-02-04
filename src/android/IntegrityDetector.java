85a299a49ea55f2bc3187c9dd481e29d11eb992c
Bingbing Huang <bing2013@cs.washington.edu>
2018-02-04 02:27:25 -0800

    bug fixed, and location request added

--- src/android/IntegrityDetector.java
+++ src/android/IntegrityDetector.java
@@ -11,11 +11,15 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
 
 import edu.berkeley.eecs.emission.cordova.unifiedlogger.Log;
 
@@ -25,18 +29,37 @@ public class IntegrityDetector extends Service implements SensorEventListener {
     Sensor linearAccelerometer;
     private final String linearAccTAG = "linearAcceleration";
 
-    ArrayList<Double> input = new ArrayList<Double>();
+    // Time in milliseconds: 30s -> 30000 milliseconds
+    private static final long timeThreshold = 30000;
+    private static final long countThreshold = 3000;
+    private long startTime;
 
-    private final double PI = 3.1415926535897932384626433832795;
-    private final double FREQ = 1.0;
-    // Unit: mm
-    private final double threshold = 50;
-    // Unit: s
-    private final long timeDuration = 30;
+    private LinkedList<float[]> rawValuesWindow;
+    private ArrayList<float[]> rawValues;
+    private ArrayList<float[]> valuesLowPass;
 
-    long startTime = 0;
-    int bumpCount = 0;
-    int resultCount = 0;
+    private LinkedList<Float> xLowPass;
+    private LinkedList<Float> yLowPass;
+    private LinkedList<Float> zLowPass;
+
+    private boolean isReady = false;
+    private boolean isInitialized = false;
+
+    private double xThreshold;
+    private double yThreshold;
+    private double zThreshold;
+
+    private ArrayList<Long> timeStampsX;
+    private ArrayList<Long> timeStampsY;
+    private ArrayList<Long> timeStampsZ;
+
+    private int numberOfRawValues = 0;
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    private double latitude;
+    private double longitude;
 
     @Nullable
     @Override
@@ -52,53 +75,207 @@ public class IntegrityDetector extends Service implements SensorEventListener {
         linearAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         sensorManager.registerListener(this, linearAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
+        startTime = System.currentTimeMillis();
+        rawValuesWindow = new LinkedList<float[]>();
+        rawValues = new ArrayList<float[]>();
+        valuesLowPass = new ArrayList<float[]>();
+
+        xLowPass = new LinkedList<Float>();
+        yLowPass = new LinkedList<Float>();
+        zLowPass = new LinkedList<Float>();
+
+        timeStampsX = new ArrayList<Long>();
+        timeStampsY = new ArrayList<Long>();
+        timeStampsZ = new ArrayList<Long>();
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 10, locationListener);
+
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-
-        if (startTime == 0) {
-            startTime = System.currentTimeMillis();
-        }
-
-        // Unit: second
-        long tmpTimeDuration = (System.currentTimeMillis() - startTime) / 1000;
-        if (tmpTimeDuration >= timeDuration) {
-//            Log.d(this, linearAccTAG, "number of results is " + resultCount);
-//            Log.d(this, linearAccTAG, "number of bumps is " + bumpCount);
-            startTime = System.currentTimeMillis();
-            bumpCount = 0;
-            resultCount = 0;
-            saveToDatabase(this, new SimpleMovementSensorEvent(sensorEvent.accuracy, bumpCount, threshold, timeDuration));
-        }
-
-        // Unit: convert m/s^2 to mm/s^2
-        float x = sensorEvent.values[0] * 1000;
-        float y = sensorEvent.values[1] * 1000;
-        float z = sensorEvent.values[2] * 1000;
-
-//        if (sensorEvent.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
-//            Log.d(this, linearAccTAG, Float.toString(x) + ", " + Float.toString(y) + ", " + Float.toString(z));
-//        }
-
-        double v = (double) (x * x + y * y + z * z);
-//        Log.d(this, linearAccTAG, "v is " + v);
-        input.add(Math.sqrt(v));
-
-        if (input.size() == 5) {
-            Double averageAccel = calculateAverage(input);
-//            Log.d(this, linearAccTAG, "average is " + averageAccel);
-
-            Double result = accel2mms(averageAccel, FREQ);
-            resultCount++;
-//            Log.d(this, linearAccTAG, "result is " + result);
-
-            if (result > threshold) {
-                bumpCount++;
+        if (!isInitialized) {
+            if (rawValuesWindow.size() < 5) {
+                rawValuesWindow.offer(sensorEvent.values.clone());
+                rawValues.add(sensorEvent.values);
+            } else if (rawValuesWindow.size() == 5 && !isReady) {
+                float[] values = getLowPassValue();
+                assignValues1(values);
+                valuesLowPass.add(values);
+                isReady = true;
             }
-            input.clear();
+
+            if (isReady) {
+                rawValuesWindow.poll();
+                rawValuesWindow.offer(sensorEvent.values.clone());
+                rawValues.add(sensorEvent.values);
+                float[] values = getLowPassValue();
+                assignValues1(values);
+                valuesLowPass.add(values);
+            }
+            long currTime = System.currentTimeMillis() - startTime;
+            if (currTime >= timeThreshold) {
+                xThreshold = getAdaptiveThreshold(xLowPass);
+                yThreshold = getAdaptiveThreshold(yLowPass);
+                zThreshold = getAdaptiveThreshold(zLowPass);
+                isInitialized = true;
+                startTime = System.currentTimeMillis();
+            }
+        } else {
+            rawValuesWindow.poll();
+            rawValuesWindow.offer(sensorEvent.values.clone());
+            numberOfRawValues++;
+            float[] values = getLowPassValue();
+            assignValues2(values);
+
+            xThreshold = getAdaptiveThreshold(xLowPass);
+            if (checkBump(values[0], xThreshold)) {
+                timeStampsX.add(System.currentTimeMillis());
+            }
+
+            yThreshold = getAdaptiveThreshold(yLowPass);
+            if (checkBump(values[1], yThreshold)) {
+                timeStampsY.add(System.currentTimeMillis());
+            }
+
+            zThreshold = getAdaptiveThreshold(zLowPass);
+            if (checkBump(values[2], zThreshold)) {
+                timeStampsZ.add(System.currentTimeMillis());
+            }
+
+            if (numberOfRawValues >= countThreshold) {
+                // If the last input is identified as bump,
+                // then the time stamps added as bumps may be greater then the time stamp we get directly from sensor data.
+                // save the output values  into the database
+                double[] resultX = new double[2];
+                double[] resultY = new double[2];
+                double[] resultZ = new double[2];
+
+                if (timeStampsX.size() != 0) {
+                    resultX = getSTDandMean(timeStampsX);
+                }
+
+                if (timeStampsY.size() != 0) {
+                    resultY = getSTDandMean(timeStampsY);
+                }
+
+                if (timeStampsZ.size() != 0) {
+                    resultZ = getSTDandMean(timeStampsZ);
+                }
+
+
+                saveToDatabase(this, new SimpleMovementSensorEvent(resultX[0], resultX[1], timeStampsX, resultY[0], resultY[1], timeStampsY, resultZ[0], resultZ[1], timeStampsZ, latitude, longitude, sensorEvent.timestamp));
+                Log.d(this, linearAccTAG, "Saved into database");
+
+                timeStampsX.clear();
+                timeStampsY.clear();
+                timeStampsZ.clear();
+            }
         }
+    }
+
+    private boolean checkBump(float lowPassValue, double threshold) {
+        return lowPassValue >= threshold;
+    }
+
+    private void assignValues1(float[] values) {
+        xLowPass.offer(values[0]);
+        yLowPass.offer(values[1]);
+        zLowPass.offer(values[2]);
+    }
+
+    private void assignValues2(float[] values) {
+        xLowPass.poll();
+        xLowPass.offer(values[0]);
+
+        yLowPass.poll();
+        yLowPass.offer(values[1]);
+
+        zLowPass.poll();
+        zLowPass.offer(values[2]);
+    }
+
+    private static float mean(LinkedList<Float> values) {
+        float sum = 0f;
+        for (Float n : values) {
+            sum += n;
+        }
+        return sum / values.size();
+    }
+
+    private static double stdv(LinkedList<Float> values, float mean) {
+        float sum = 0f;
+        for (Float n : values) {
+            sum += Math.pow(n - mean, 2);
+        }
+        return Math.sqrt(sum / values.size());
+    }
+
+    private static double[] getSTDandMean(ArrayList<Long> timeStamps) {
+        double sum = 0.0;
+        for (Long l : timeStamps) {
+            sum += l;
+        }
+        double mean = sum / timeStamps.size();
+
+        double sum_std = 0.0;
+        for (Long l : timeStamps) {
+            sum_std += Math.pow(l - mean, 2);
+        }
+        double std = Math.sqrt(sum / timeStamps.size());
+
+        return new double[]{mean, std};
+    }
+
+    private double getAdaptiveThreshold(LinkedList<Float> lowPassValues) {
+        float mean = mean(lowPassValues);
+        double stdv = stdv(lowPassValues, mean);
+        return mean + 3 * stdv;
+    }
+
+    private float[] getLowPassValue() {
+        float[] output = new float[3];
+        float[] input0 = rawValuesWindow.get(0);
+        float test[] = new float[3];
+        try {
+            test = rawValuesWindow.get(1);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        // System.out.println(test[0]);
+        float[] input1 = rawValuesWindow.get(1);
+        float[] input2 = rawValuesWindow.get(2);
+        float[] input3 = rawValuesWindow.get(3);
+        float[] input4 = rawValuesWindow.get(4);
+
+        for (int i = 0; i < input0.length; i++) {
+            output[i] = 0.1f * input0[i] + 0.2f * input1[i] + 0.5f * input2[i] + 0.2f * input3[i] + 0.1f * input4[i];
+        }
+        return output;
     }
 
     @Override
@@ -121,25 +298,5 @@ public class IntegrityDetector extends Service implements SensorEventListener {
         Log.d(this, destoryTAG, "onDestroy called");
         super.onDestroy();
         sensorManager.unregisterListener(this);
-    }
-
-    private double calculateAverage(List<Double> list) {
-        Double sum = 0.0;
-        if (!list.isEmpty()) {
-            for (Double n : list) {
-                sum += n;
-            }
-            return sum / list.size();
-        }
-        return sum;
-    }
-
-    /*
-        This calculation is based on a sinusoidal waveform,
-        so the frequency would be representative of the physical movement of the accelerometer
-        not the frequency of the sampling rate.
-     */
-    private double accel2mms(double accel, double freq) {
-        return accel / (2 * PI * freq);
     }
 }
--- src/android/SimpleMovementSensorEvent.java
+++ src/android/SimpleMovementSensorEvent.java
@@ -1,36 +1,87 @@
 package edu.berkeley.eecs.emission.cordova.integrityDetect;
 
+import java.util.ArrayList;
+
 public class SimpleMovementSensorEvent {
 
-    public float getAccuracy() {
-        return accuracy;
+    public double getSTDX() {
+        return stdX;
+    }
+    public double getMeanX() {
+        return meanX;
+    }
+    public ArrayList getTimeStampsX() {
+        return timeStampsX;
     }
 
-    public int getNumberOfBumps() {
-        return numberOfBumps;
+    public double getSTDY() {
+        return stdY;
+    }
+    public double getMeanY() {
+        return meanY;
+    }
+    public ArrayList getTimeStampsY() {
+        return timeStampsY;
     }
 
-    public double getThershold() {
-        return thershold;
+    public double getSTDZ() {
+        return stdZ;
+    }
+    public double getMeanZ() {
+        return meanZ;
+    }
+    public ArrayList getTimeStampsZ() {
+        return timeStampsZ;
     }
 
-    public long getTimeDuration() {
-        return timeDuration;
+    public double getLatitude() {
+        return latitude;
+    }
+    public double getLongitude() {
+        return longitude;
     }
 
-    private int accuracy;
-    private int numberOfBumps;
-    private double thershold;
-    private long timeDuration;
+    public double getFinalTimeStamp() {
+        return finalTimeStamp;
+    }
+
+    private double stdX;
+    private double meanX;
+    private ArrayList timeStampsX;
+
+    private double stdY;
+    private double meanY;
+    private ArrayList timeStampsY;
+    
+    private double stdZ;
+    private double meanZ;
+    private ArrayList timeStampsZ;
+
+    private double latitude;
+    private double longitude;
+
+    private double finalTimeStamp;
 
 
     public SimpleMovementSensorEvent() {
     }
 
-    public SimpleMovementSensorEvent(int accuracy, int numberOfBumps, double thershold, long timeDuration) {
-        this.accuracy = accuracy;
-        this.numberOfBumps = numberOfBumps;
-        this.thershold = thershold;
-        this.timeDuration = timeDuration;
+    public SimpleMovementSensorEvent(double stdX, double meanX, ArrayList timeStampsX, double stdY, double meanY, ArrayList timeStampsY, double stdZ, double meanZ, ArrayList timeStampsZ, double latitude, double longitude, double finalTimeStamp) {
+        this.stdX = stdX;
+        this.meanX = meanX;
+        this.timeStampsX = timeStampsX;
+
+        this.stdY = stdY;
+        this.meanY = meanY;
+        this.timeStampsY = timeStampsY;
+
+        this.stdZ = stdZ;
+        this.meanZ = meanZ;
+        this.timeStampsZ = timeStampsZ;
+        
+        this.latitude = latitude;
+        this.longitude = longitude;
+
+        this.finalTimeStamp = finalTimeStamp;
     }
 }
